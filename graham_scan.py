93289749114ef730ffc16c9f5008984ae642254b
Jani Rahkola <jani.rahkola@gmail.com>
2010-06-09 21:58:52 +0300

    everything looks ready

--- graham_scan.py
+++ graham_scan.py
@@ -1,8 +1,9 @@
 import vertex_list
 import svg
+import sys
 
 if __name__ == "__main__":
-    list = vertex_list.VertexList('input')
+    list = vertex_list.VertexList(sys.argv[1])
     svg = svg.SVG('test.svg')
     for p in list:
         svg.draw_vertex(p)
--- vertex_list.py
+++ vertex_list.py
@@ -1,7 +1,7 @@
-
 class VertexList(list):
-    """A list of (x, y) vertices. Provides a specialized methods for
-    Graham scan implementation.
+    """A list of (x, y) vertices representing a vertex set.
+    
+    Provides a specialized methods for Graham scan implementation.
 
     Instance variables:
         x_min
@@ -61,10 +61,12 @@ class VertexList(list):
                 return i
         raise ValueError('%s not in list'%(x))
 
-    # FIXME
-    def __cmp_value_function(self, x):
+    def __cmp_value(self, x):
         """Return the contangent of the angle between the x-axis and a line via
         x and the vertex with the lowest y coordinate.
+
+        Arguments:
+            x -- a vertex in R^2
 
         Exceptions:
             TypeError
@@ -78,6 +80,38 @@ class VertexList(list):
             else:
                 return float('-inf')
 
+    def __cmp_function(self, x, y):
+        """Compare x to y.
+
+        Primary comparison is done based on the polar angle of the vertex. Secondary
+        comparison is done based on the x coordinates.
+
+        Arguments:
+            x -- a vertex in R^2
+            y -- a vertex in R^2
+
+        Exceptions:
+            TypeError
+
+        Returns:
+            Negative, zero or positive value depending whether x is smaller,
+            equal or greater than y.
+
+        """
+        x_polar = self.__cmp_value(x)
+        y_polar = self.__cmp_value(y)
+
+        if x_polar < y_polar:
+            return 1
+        elif x_polar > y_polar:
+            return -1
+        elif x[0] < y[0]:
+            return -1
+        elif x[0] > y[0]:
+            return 1
+        else:
+            return 0
+
     def graham_sort(self):
         """Sort vertices to increasing order.
 
@@ -86,25 +120,56 @@ class VertexList(list):
 
         """
         i = self.index(self.lowest_y)
+        print self.lowest_y
         self[0], self[i] = self[i], self[0]
-        list.sort(self, key=self.__cmp_value_function, reverse=True)
-        # self.__quicksort()
+        print self
+        # list.sort(self, cmp=self.__cmp_function)
+        self.__quicksort(0, len(self)-1)
+        print self
 
     def convex_hull(self):
-        def is_ccw(p1, p2, p3):
+        """Calculate the convex hull of the vertex set.
+
+        Convex hull is calculated using the Graham scan algorithm.
+        Innerfunction ccw is used to determine whether theree vertices make a
+        left or a right turn.
+
+        Exceptions:
+            TypeError
+
+        Returns:
+            A list of vertecies that represen the convex hull of the vertex
+            set.
+            
+        """
+        def ccw(p1, p2, p3):
             return (p2[0] - p1[0])*(p3[1] - p1[1]) - (p2[1] - p1[1])*(p3[0] - p1[0]) <= 0
 
-        if len(self) <= 2:
+        if len(self) <= 3:
             return self
 
         self.graham_sort()
         m = 1
         for i in range(2, len(self)):
-            while is_ccw(self[m-1], self[m], self[i]) and m > 0:
+            while m > 0 and ccw(self[m-1], self[m], self[i]):
                 m -= 1
             m += 1
             self[m], self[i] = self[i], self[m]
         return self[:m+1]
 
-    def __quicksort(self):
-        pass
+    def __quicksort(self, left, right):
+        if right > left:
+            split = self.__partition(left, right, (left+right)/2)
+            self.__quicksort(left, split-1)
+            self.__quicksort(split+1, right)
+
+    def __partition(self, left, right, pivot):
+        self[pivot], self[right] = self[right], self[pivot]
+        split = left
+        for i in range(left, right):
+            if self.__cmp_function(self[i], self[right]) <= 0:
+                self[i], self[split] = self[split], self[i]
+                split += 1
+        self[split], self[right] = self[right], self[split]
+        return split
+
